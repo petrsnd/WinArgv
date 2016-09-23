44a40d6d1b90832f4823d65aab3ef25180e20b60
petrsnd <petrsnd@gmail.com>
2016-09-23 16:41:33 -0600

    Adding an external execution to really test args
    The internal API doesn't seem to follow Microsoft's own rules.

---
+++ CommandLineTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CommandLineTester
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
